ac53e563d8831060477099e2fa236687b1631733
AdrianCervantes98 <is708376@iteso.mx>
2021-03-19 12:45:38 -0600

    FrontEnd Post method not working yet

--- AppFrontEnd/src/routes/index.js
+++ AppFrontEnd/src/routes/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const https = require('https');
+const axios = require('axios');
 
 const helloRoute = require('./hello');
 
@@ -8,9 +10,26 @@ router.get('/autor', (req, res) => {
     res.send({ alumno: "AACA", servicio: "EKS en AWS"})
 });
 
+//https://appibm.us-south.cf.appdomain.cloud/analyze
+
 router.post('/analyze', (req, res) => {
-    export const input = req.body.inputText;
-    console.log(input);
+    //const input = req.body.inputText;
+    const input = JSON.stringify({
+        receivedText: req.body.inputText,
+    })
+    console.log(input)
+    let result = doRequest(input);
+    result.then(function(response){
+        console.log(response)
+    })
 });
+
+async function doRequest(payload) {
+    console.log("entré a la func");
+    let res = await axios.post("https://appibm.us-south.cf.appdomain.cloud/analyze", payload);
+    return res.data;
+}
+
+
 
 module.exports = router;
